7d69ed84f49c283200790022228b404b43bebc07
Lorenzo Cattaneo <info@lorenzocattaneo.com>
2021-01-28 12:39:08 +0100

    added missing translations

--- lang/en/drystack.php
+++ lang/en/drystack.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
+    'auth.name' => 'Name',
     'auth.email' => 'Email',
     'auth.password' => 'Password',
+    'auth.password.current' => 'Current password',
     'auth.password.confirm' => 'Confirm password',
     'auth.forgot' => 'Forgot your password?',
     'auth.remember' => 'Remember me',
@@ -20,8 +22,15 @@ return [
     'users' => 'Users',
 
     'title.dashboard' => 'Dashboard',
+    'title.profile' => 'My profile',
     'title.user.index' => 'Users',
     'title.user.create' => 'New',
     'title.user.update' => 'Update :name',
-    'title.user.view' => ':name'
+    'title.user.view' => ':name',
+
+
+    'profile.edit.title' => 'Update personal data',
+    'profile.edit.description' => 'Update your personal data. The chosen email will be your login email.',
+    'profile.password.title' => 'Update password',
+    'profile.password.description' => 'Type your current password to confirm. The new password cannot be the same as the current one.',
 ];
--- lang/it/drystack.php
+++ lang/it/drystack.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
+    'auth.name' => 'Nome',
     'auth.email' => 'Email',
     'auth.password' => 'Password',
+    'auth.password.current' => 'Password attuale',
     'auth.password.confirm' => 'Conferma password',
     'auth.forgot' => 'Password dimenticata?',
     'auth.remember' => 'Ricordami',
@@ -20,8 +22,14 @@ return [
     'users' => 'Utenti',
 
     'title.dashboard' => 'Dashboard',
+    'title.profile' => 'Il mio profilo',
     'title.user.index' => 'Utenti',
     'title.user.create' => 'Crea',
     'title.user.update' => 'Aggiorna :name',
-    'title.user.view' => ':name'
+    'title.user.view' => ':name',
+
+    'profile.edit.title' => 'Modifica dati personali',
+    'profile.edit.description' => 'Aggiorna il tuo nome ed email. La email inserita sarà usata per il login.',
+    'profile.password.title' => 'Aggiorna la tua password',
+    'profile.password.description' => 'Inserisci la tua password attuale per confermare. La nuova password non può essere uguale a quella attuale.',
 ];
